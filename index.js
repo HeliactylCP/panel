1e77598cc94bc52554768f18c5f9c9e9db7a3b27
Plexed <64319639+pl3xed@users.noreply.github.com>
2022-01-16 15:27:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ const listener = app.listen(settings.website.port, function() {
   console.log(chalk.green("[Heliactyl] Finishing & deploying Heliactyl..."));
   console.log(chalk.green("----------------------------------------------------"));
   console.log(chalk.green("Heliactyl: v11 - Kanjut"));
-  console.log(chalk.green("Release: v11.1.2"));
+  console.log(chalk.green("Release: v11.2.0"));
   console.log(chalk.green("----------------------------------------------------"));
   console.log(chalk.green("Your dashboard will now be available on port " + listener.address().port + " "));
   console.log(chalk.green("----------------------------------------------------"));
