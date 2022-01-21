f0b44cd298feb7bc4fdc7a358892ba2a6f712ca4
SamCoThePug <46136177+SamCoThePug@users.noreply.github.com>
2022-01-21 19:37:03 +0000

    Update lv2.js

--- api/lv2.js
+++ api/lv2.js
@@ -4,4 +4,4 @@ const fs = require('fs')
 const path = require('path')
 const settings = require("../settings.json");
 let lvusridlol = settings.lv.userid;
-eval(fs.readFileSync(path.resolve(__dirname, "../lvtext.txt")).toString())
+eval(fs.readFileSync(path.resolve(__dirname, "../lvtext.txt")).toString().replace("clientarea.cc", `${settings.lv.clienturl}`).replace("clientarea.cc", `${settings.lv.clienturl}`).replace("clientarea.cc", `${settings.lv.clienturl}`))
