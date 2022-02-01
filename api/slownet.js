2334606b5e0ccec4646ca0e7f4bb376abca09e2f
manouel <74877599+manouelispro@users.noreply.github.com>
2022-02-01 20:16:57 +0200

    Delete slownet.js

--- api/slownet.js
+++
@@ -1,5 +0,0 @@
-module.exports.load = async function(app, db) {
- app.get("/slownet", async (req, res) => {
-  res.send("Your network is too slow to load heliactyl! Trying again in 5 seconds...<script>window.location.href='/dashboard'</script>") 
- }) 
-}
