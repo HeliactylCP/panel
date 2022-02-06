b24d03e595c1dcb8d817810426fec47a8750b819
manouel <74877599+manouelispro@users.noreply.github.com>
2022-02-06 12:11:38 +0200

    Update servers.js

--- api/servers.js
+++ api/servers.js
@@ -10,8 +10,8 @@ if (settings.pterodactyl) if (settings.pterodactyl.domain) {
   if (settings.pterodactyl.domain.slice(-1) == "/") settings.pterodactyl.domain = settings.pterodactyl.domain.slice(0, -1);
 };
 
-module.exports.load = async function(app, db) {	
-  app.get("/updateinfo", async (req, res) => {	
+module.exports.load = async function(app, db) {
+  app.get("/updateinfo", async (req, res) => {
     if (!req.session.pterodactyl) return res.redirect("/login")
     let cacheaccount = await fetch(
       settings.pterodactyl.domain + "/api/application/users/" + (await db.get("users-" + req.session.userinfo.id)) + "?include=servers",
@@ -25,38 +25,37 @@ module.exports.load = async function(app, db) {
     req.session.pterodactyl = cacheaccountinfo.attributes;
     if (req.query.redirect) if (typeof req.query.redirect == "string") return res.redirect("/" + req.query.redirect);
     let theme = indexjs.get(req);
-    res.redirect("/settings");
+    res.redirect(theme.settings.redirect.updateservers || "/");
   });
 
   app.get("/create", async (req, res) => {
     if (!req.session.pterodactyl) return res.redirect("/login");
-
+    
     let theme = indexjs.get(req);
 
     let newsettings = JSON.parse(fs.readFileSync("./settings.json").toString());
     if (newsettings.api.client.allow.server.create == true) {
-      let redirectlink = theme.settings.redirect.failedcreateserver ? theme.settings.redirect.failedcreateserver : "/"; // fail redirect link
-
+      let redirectlink = theme.settings.redirect.failedcreateserver || "/"; // fail redirect link
+      
       if (req.query.name && req.query.ram && req.query.disk && req.query.cpu && req.query.egg && req.query.location) {
         try {
           decodeURIComponent(req.query.name)
-        } catch (err) {
+        } catch(err) {
           return res.redirect(`${redirectlink}?err=COULDNOTDECODENAME`);
         }
 
         let packagename = await db.get("package-" + req.session.userinfo.id);
         let package = newsettings.api.client.packages.list[packagename ? packagename : newsettings.api.client.packages.default];
 
-        let extra =
-          await db.get("extra-" + req.session.userinfo.id) ?
-            await db.get("extra-" + req.session.userinfo.id) :
-            {
-              ram: 0,
-              disk: 0,
-              cpu: 0,
-              servers: 0
-            };
-        let j4r =
+        let extra = 
+        await db.get("extra-" + req.session.userinfo.id) ||
+          {
+            ram: 0,
+            disk: 0,
+            cpu: 0,
+            servers: 0
+          };
+          let j4r =
           await db.get("j4r-" + req.session.userinfo.id) ?
             await db.get("j4r-" + req.session.userinfo.id) :
             {
@@ -81,7 +80,7 @@ module.exports.load = async function(app, db) {
         let name = decodeURIComponent(req.query.name);
         if (name.length < 1) return res.redirect(`${redirectlink}?err=LITTLESERVERNAME`);
         if (name.length > 191) return res.redirect(`${redirectlink}?err=BIGSERVERNAME`);
-
+  
         let location = req.query.location;
 
         if (Object.entries(newsettings.api.client.locations).filter(vname => vname[0] == location).length !== 1) return res.redirect(`${redirectlink}?err=INVALIDLOCATION`);
@@ -91,16 +90,16 @@ module.exports.load = async function(app, db) {
 
 
         let egg = req.query.egg;
-
+  
         let egginfo = newsettings.api.client.eggs[egg];
         if (!newsettings.api.client.eggs[egg]) return res.redirect(`${redirectlink}?err=INVALIDEGG`);
         let ram = parseFloat(req.query.ram);
         let disk = parseFloat(req.query.disk);
         let cpu = parseFloat(req.query.cpu);
         if (!isNaN(ram) && !isNaN(disk) && !isNaN(cpu)) {
-          if (ram2 + ram > package.ram + extra.ram + j4r.ram) return res.redirect(`${redirectlink}?err=EXCEEDRAM&num=${package.ram + extra.ram + j4r.ram - ram2}`);
-          if (disk2 + disk > package.disk + extra.disk + j4r.disk) return res.redirect(`${redirectlink}?err=EXCEEDDISK&num=${package.disk + extra.disk + j4r.disk - disk2}`);
-          if (cpu2 + cpu > package.cpu + extra.cpu + j4r.cpu) return res.redirect(`${redirectlink}?err=EXCEEDCPU&num=${package.cpu + extra.cpu + j4r.cpu - cpu2}`);
+          if (ram2 + ram > package.ram + extra.ram + j4r.ram) return res.redirect(`${redirectlink}?id=${req.query.id}&err=EXCEEDRAM&num=${package.ram + extra.ram + j4r.ram - ram2}`);
+        if (disk2 + disk > package.disk + extra.disk + j4r.disk) return res.redirect(`${redirectlink}?id=${req.query.id}&err=EXCEEDDISK&num=${package.disk + extra.disk + j4r.disk - disk2}`);
+        if (cpu2 + cpu > package.cpu + extra.cpu + j4r.cpu) return res.redirect(`${redirectlink}?id=${req.query.id}&err=EXCEEDCPU&num=${package.cpu + extra.cpu + j4r.cpu - cpu2}`);
           if (egginfo.minimum.ram) if (ram < egginfo.minimum.ram) return res.redirect(`${redirectlink}?err=TOOLITTLERAM&num=${egginfo.minimum.ram}`);
           if (egginfo.minimum.disk) if (disk < egginfo.minimum.disk) return res.redirect(`${redirectlink}?err=TOOLITTLEDISK&num=${egginfo.minimum.disk}`);
           if (egginfo.minimum.cpu) if (cpu < egginfo.minimum.cpu) return res.redirect(`${redirectlink}?err=TOOLITTLECPU&num=${egginfo.minimum.cpu}`);
@@ -109,7 +108,7 @@ module.exports.load = async function(app, db) {
             if (egginfo.maximum.disk) if (disk > egginfo.maximum.disk) return res.redirect(`${redirectlink}?err=TOOMUCHDISK&num=${egginfo.maximum.disk}`);
             if (egginfo.maximum.cpu) if (cpu > egginfo.maximum.cpu) return res.redirect(`${redirectlink}?err=TOOMUCHCPU&num=${egginfo.maximum.cpu}`);
           }
-
+  
           let specs = egginfo.info;
           specs["user"] = (await db.get("users-" + req.session.userinfo.id));
           if (!specs["limits"]) specs["limits"] = {
@@ -127,7 +126,7 @@ module.exports.load = async function(app, db) {
             port_range: []
           }
           specs.deploy.locations = [location];
-
+  
           let serverinfo = await fetch(
             settings.pterodactyl.domain + "/api/application/servers",
             {
@@ -165,7 +164,7 @@ module.exports.load = async function(app, db) {
                 body: params
             }).catch(e => console.warn(chalk.red("[WEBSITE] There was an error sending to the webhook: " + e)));
         }
-          return res.redirect(theme.settings.redirect.createserver ? theme.settings.redirect.createserver : "/");
+          return res.redirect(theme.settings.redirect.createserver || "/");
         } else {
           res.redirect(`${redirectlink}?err=NOTANUMBER`);
         }
@@ -173,7 +172,7 @@ module.exports.load = async function(app, db) {
         res.redirect(`${redirectlink}?err=MISSINGVARIABLE`);
       }
     } else {
-      res.redirect(theme.settings.redirect.createserverdisabled ? theme.settings.redirect.createserverdisabled : "/");
+      res.redirect(theme.settings.redirect.createserverdisabled || "/");
     }
   });
 
@@ -186,23 +185,23 @@ module.exports.load = async function(app, db) {
     if (newsettings.api.client.allow.server.modify == true) {
       if (!req.query.id) return res.send("Missing server id.");
 
-      let redirectlink = theme.settings.redirect.failedmodifyserver ? theme.settings.redirect.failedmodifyserver : "/"; // fail redirect link
-
+      let redirectlink = theme.settings.redirect.failedmodifyserver || "/"; // fail redirect link
+  
       let checkexist = req.session.pterodactyl.relationships.servers.data.filter(name => name.attributes.id == req.query.id);
       if (checkexist.length !== 1) return res.send("Invalid server id.");
-
+  
       let ram = req.query.ram ? (isNaN(parseFloat(req.query.ram)) ? undefined : parseFloat(req.query.ram)) : undefined;
       let disk = req.query.disk ? (isNaN(parseFloat(req.query.disk)) ? undefined : parseFloat(req.query.disk)) : undefined;
       let cpu = req.query.cpu ? (isNaN(parseFloat(req.query.cpu)) ? undefined : parseFloat(req.query.cpu)) : undefined;
-
+  
       if (ram || disk || cpu) {
         let newsettings = JSON.parse(fs.readFileSync("./settings.json").toString());
-
+  
         let packagename = await db.get("package-" + req.session.userinfo.id);
         let package = newsettings.api.client.packages.list[packagename ? packagename : newsettings.api.client.packages.default];
-
+  
         let pterorelationshipsserverdata = req.session.pterodactyl.relationships.servers.data.filter(name => name.attributes.id.toString() !== req.query.id);
-
+  
         let ram2 = 0;
         let disk2 = 0;
         let cpu2 = 0;
@@ -213,7 +212,7 @@ module.exports.load = async function(app, db) {
         }
         let attemptegg = null;
         //let attemptname = null;
-
+        
         for (let [name, value] of Object.entries(newsettings.api.client.eggs)) {
           if (value.info.egg == checkexist[0].attributes.egg) {
             attemptegg = newsettings.api.client.eggs[name];
@@ -221,31 +220,27 @@ module.exports.load = async function(app, db) {
           };
         };
         let egginfo = attemptegg ? attemptegg : null;
-
+  
         if (!egginfo) return res.redirect(`${redirectlink}?id=${req.query.id}&err=MISSINGEGG`);
 
-        let extra =
-          await db.get("extra-" + req.session.userinfo.id) ?
-            await db.get("extra-" + req.session.userinfo.id) :
+        let extra = await db.get("extra-" + req.session.userinfo.id) ||
+          {
+            ram: 0,
+            disk: 0,
+            cpu: 0,
+            servers: 0
+          };
+          let j4r = await db.get("j4r-" + req.session.userinfo.id) ||
             {
               ram: 0,
               disk: 0,
               cpu: 0,
               servers: 0
             };
-        let j4r =
-          await db.get("j4r-" + req.session.userinfo.id) ?
-            await db.get("j4r-" + req.session.userinfo.id) :
-            {
-              ram: 0,
-              disk: 0,
-              cpu: 0,
-              servers: 0
-            };
-
-        if (ram2 + ram > package.ram + extra.ram + j4r.ram) return res.redirect(`${redirectlink}?id=${req.query.id}&err=EXCEEDRAM&num=${package.ram + extra.ram + j4r.ram - ram2}`);
-        if (disk2 + disk > package.disk + extra.disk + j4r.disk) return res.redirect(`${redirectlink}?id=${req.query.id}&err=EXCEEDDISK&num=${package.disk + extra.disk + j4r.disk - disk2}`);
-        if (cpu2 + cpu > package.cpu + extra.cpu + j4r.cpu) return res.redirect(`${redirectlink}?id=${req.query.id}&err=EXCEEDCPU&num=${package.cpu + extra.cpu + j4r.cpu - cpu2}`);
+  
+            if (ram2 + ram > package.ram + extra.ram + j4r.ram) return res.redirect(`${redirectlink}?id=${req.query.id}&err=EXCEEDRAM&num=${package.ram + extra.ram + j4r.ram - ram2}`);
+            if (disk2 + disk > package.disk + extra.disk + j4r.disk) return res.redirect(`${redirectlink}?id=${req.query.id}&err=EXCEEDDISK&num=${package.disk + extra.disk + j4r.disk - disk2}`);
+            if (cpu2 + cpu > package.cpu + extra.cpu + j4r.cpu) return res.redirect(`${redirectlink}?id=${req.query.id}&err=EXCEEDCPU&num=${package.cpu + extra.cpu + j4r.cpu - cpu2}`);
         if (egginfo.minimum.ram) if (ram < egginfo.minimum.ram) return res.redirect(`${redirectlink}?id=${req.query.id}&err=TOOLITTLERAM&num=${egginfo.minimum.ram}`);
         if (egginfo.minimum.disk) if (disk < egginfo.minimum.disk) return res.redirect(`${redirectlink}?id=${req.query.id}&err=TOOLITTLEDISK&num=${egginfo.minimum.disk}`);
         if (egginfo.minimum.cpu) if (cpu < egginfo.minimum.cpu) return res.redirect(`${redirectlink}?id=${req.query.id}&err=TOOLITTLECPU&num=${egginfo.minimum.cpu}`);
@@ -254,7 +249,7 @@ module.exports.load = async function(app, db) {
           if (egginfo.maximum.disk) if (disk > egginfo.maximum.disk) return res.redirect(`${redirectlink}?id=${req.query.id}&err=TOOMUCHDISK&num=${egginfo.maximum.disk}`);
           if (egginfo.maximum.cpu) if (cpu > egginfo.maximum.cpu) return res.redirect(`${redirectlink}?id=${req.query.id}&err=TOOMUCHCPU&num=${egginfo.maximum.cpu}`);
         };
-
+  
         let limits = {
           memory: ram ? ram : checkexist[0].attributes.limits.memory,
           disk: disk ? disk : checkexist[0].attributes.limits.disk,
@@ -262,7 +257,7 @@ module.exports.load = async function(app, db) {
           swap: egginfo ? checkexist[0].attributes.limits.swap : 0,
           io: egginfo ? checkexist[0].attributes.limits.io : 500
         };
-
+  
         let serverinfo = await fetch(
           settings.pterodactyl.domain + "/api/application/servers/" + req.query.id + "/build",
           {
@@ -281,17 +276,35 @@ module.exports.load = async function(app, db) {
         req.session.pterodactyl.relationships.servers.data = pterorelationshipsserverdata;
         let theme = indexjs.get(req);
         adminjs.suspend(req.session.userinfo.id);
-        res.redirect(theme.settings.redirect.modifyserver ? theme.settings.redirect.modifyserver : "/");
+        if(newsettings.api.client.webhook.auditlogs.enabled && !newsettings.api.client.webhook.auditlogs.disabled.includes("SERVER")) {
+          let params = JSON.stringify({
+              embeds: [
+                  {
+                      title: "Server Modified",
+                      description: `**__User:__** ${req.session.userinfo.username}#${req.session.userinfo.discriminator} (${req.session.userinfo.id})\n\n**__New Configuration:__**\n${checkexist[0].attributes.limits.memory}MB Ram\n${checkexist[0].attributes.limits.disk}MB Disk\n${checkexist[0].attributes.limits.cpu}% CPU`,
+                      color: hexToDecimal("#ffff00")
+                  }
+              ]
+          })
+          fetch(`${newsettings.api.client.webhook.webhook_url}`, {
+              method: "POST",
+              headers: {
+                  'Content-type': 'application/json',
+              },
+              body: params
+          }).catch(e => console.warn(chalk.red("[WEBSITE] There was an error sending to the webhook: " + e)));
+      }
+        res.redirect(theme.settings.redirect.modifyserver || "/");
       } else {
         res.redirect(`${redirectlink}?id=${req.query.id}&err=MISSINGVARIABLE`);
       }
     } else {
-      res.redirect(theme.settings.redirect.modifyserverdisabled ? theme.settings.redirect.modifyserverdisabled : "/");
+      res.redirect(theme.settings.redirect.modifyserverdisabled || "/");
     }
   });
 
   app.get("/delete", async (req, res) => {
-    if (!req.session.pterodactyl) return res.redirect("/login");
+    if (!req.session.pterodactyl) return res.redirect("/login")
 
     if (!req.query.id) return res.send("Missing id.");
 
@@ -323,13 +336,32 @@ module.exports.load = async function(app, db) {
 
       adminjs.suspend(req.session.userinfo.id);
 
-      return res.redirect(theme.settings.redirect.deleteserver ? theme.settings.redirect.deleteserver : "/");
+      res.redirect(theme.settings.redirect.deleteserver || "/");
+
+      if(newsettings.api.client.webhook.auditlogs.enabled && !newsettings.api.client.webhook.auditlogs.disabled.includes("SERVER")) {
+        let params = JSON.stringify({
+            embeds: [
+                {
+                    title: "Server Deleted",
+                    description: `**__User:__** ${req.session.userinfo.username}#${req.session.userinfo.discriminator} (${req.session.userinfo.id})\n\n**ID:** ${req.query.id}`,
+                    color: hexToDecimal("#ffff00")
+                }
+            ]
+        })
+        fetch(`${newsettings.api.client.webhook.webhook_url}`, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: params
+        }).catch(e => console.warn(chalk.red("[WEBSITE] There was an error sending to the webhook: " + e)));
+    }
+  
     } else {
-      res.redirect(theme.settings.redirect.deleteserverdisabled ? theme.settings.redirect.deleteserverdisabled : "/");
+      res.redirect(theme.settings.redirect.deleteserverdisabled || "/");
     }
   });
 };
-
 function hexToDecimal(hex) {
   return parseInt(hex.replace("#",""), 16)
 }
